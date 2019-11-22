42befd69890bccc311f3912aa049397fb4ed61d7
Vladislav Novitskiy <novlad26@gmail.com>
2019-11-23 02:56:58 +0300

    Wrote mode; added picture

--- AESEncryptionMode/AESEncryptionMode.cpp
+++ AESEncryptionMode/AESEncryptionMode.cpp
@@ -3,10 +3,13 @@
 
 #include "pch.h"
 #include "CImg-2.4.1/CImg.h"
-#include "AES/AES.h"
+#include "AES.h"
 #include <iostream>
+#include <Windows.h>
+#include <wincrypt.h>
 
-#define BLOCK_SIZE 128
+#define BLOCK_SIZE 16
+#define BLOCK_SIZE_BYTES 128
 
 #define COLORS_NUMBER 3
 #define SQUARE 2
@@ -15,113 +18,184 @@
 
 typedef cimg_library::CImg<unsigned char> Image;
 
+struct cbc_data {
+	unsigned char* bytes;
+	unsigned char* iv;
+};
+
+unsigned char* getImageBytes(Image image);
+void saveImage(Image image, unsigned char* bytes, string path);
+unsigned char* generate();
+unsigned char* encryptECB(unsigned char* text, unsigned int size, unsigned char* key);
+unsigned char* decryptECB(unsigned char* ciphertext, unsigned int size, unsigned char* key);
+cbc_data encryptCBC(unsigned char* chyphertext, unsigned int size, unsigned char* key);
+unsigned char* decryptCBC(cbc_data cbc, unsigned int size, unsigned char* key);
+unsigned char** splitTextToBlocks(unsigned char* text, unsigned int blocks_number);
+
+__declspec(selectany) cbc_data currResult;
+
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Image initial("initial.bmp");
+	int size = initial.size();
+	int width = initial.width();
+	int height = initial.height();
+	
+	unsigned char* bytes = getImageBytes(initial);
+	unsigned char* key = generate();
+	/*
+	unsigned char* encrypted_ECB_bytes = encryptECB(bytes, size, key);
+	Image encrypted_ECB(width, height, ONE_DIMENSIONAL, COLORS_NUMBER);
+	saveImage(encrypted_ECB, encrypted_ECB_bytes, "encrypted_ECB.bmp");
+	delete(encrypted_ECB);
+
+	Image encrypted_ECB_try_do_decrypt("encrypted_ECB.bmp");
+	unsigned char* encrypted_ECB_try_do_decrypt_bytes = getImageBytes(encrypted_ECB_try_do_decrypt);
+	unsigned char* decrypted_ECB_bytes = decryptECB(encrypted_ECB_try_do_decrypt_bytes, size, key);
+	Image decrypted_ECB(width, height, ONE_DIMENSIONAL, COLORS_NUMBER);
+	saveImage(decrypted_ECB, decrypted_ECB_bytes, "decrypted_ECB.bmp");
+	delete(decrypted_ECB);
+	*/
+	currResult = encryptCBC(bytes, size, key);
+	Image encrypted_CBC(width, height, ONE_DIMENSIONAL, COLORS_NUMBER);
+	saveImage(encrypted_CBC, currResult.bytes, "encrypted_CBC.bmp");
+	delete(encrypted_CBC);
+
+	unsigned char* decrypted_CBC_bytes = decryptCBC(currResult, size, key);
+	Image decrypted_CBC(width, height, ONE_DIMENSIONAL, COLORS_NUMBER);
+	saveImage(decrypted_CBC, decrypted_CBC_bytes, "decrypted_CBC.bmp");
+	delete(decrypted_CBC);
 }
 
-unsigned char* loadImage(string path) {
-	Image image(path.c_str());
+unsigned char* getImageBytes(Image image) {
+	unsigned char* bytes = new unsigned char[image.size()];
 
-	int image_size = image.size();
-	unsigned char* bytes = new unsigned char[image_size];
-
-	for (unsigned int i = 0; i < image_size; i++) {
-		bytes[i] = image.at(i);
+	int i = 0;
+	for (int x = 0; x < image.width(); x++) {
+		for (int y = 0; y < image.height(); y++) {
+			for (int color = 0; color < COLORS_NUMBER; color++) {
+				bytes[i] = image(x, y, Z_VALUE, color);
+				i++;
+			}
+		}
 	}
 
 	return bytes;
 }
 
-void saveImage(unsigned char* text, string path) {
-	Image image(text, sizeof(text));
+void saveImage(Image image, unsigned char* bytes, string path) {
 	image.fill(0);
+
+	int i = 0;
+	for (int x = 0; x < image.width(); x++) {
+		for (int y = 0; y < image.height(); y++) {
+			for (int color = 0; color < COLORS_NUMBER; color++) {
+				image(x, y, Z_VALUE, color) = bytes[i];
+				i++;
+			}
+		}
+	}
+
 	image.save_bmp(path.c_str());
 }
 
-unsigned char* generateKey() {
+unsigned char* generate() {
+	BYTE lpGoop[BLOCK_SIZE];
+	HCRYPTPROV m_hProv;
+	m_hProv = NULL;
+	::CryptAcquireContext(&m_hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+	if (m_hProv == NULL)
+		throw GetLastError();
+	if (!CryptGenRandom(m_hProv, BLOCK_SIZE, lpGoop))
+		printf("\r\nerror");
+	if (m_hProv) ::CryptReleaseContext(m_hProv, 0);
 
+	unsigned char* res = new unsigned char[BLOCK_SIZE];
+	memcpy(res, lpGoop, BLOCK_SIZE);
+
+	return res;
 }
 
-unsigned char* encryptECB(unsigned char* text, unsigned char* key) {
-	AES aes(128);
+unsigned char* encryptECB(unsigned char* text, unsigned int size, unsigned char* key) {
+	AES aes(BLOCK_SIZE_BYTES);
 	unsigned int outLen = 0;
-	return aes.EncryptECB(text, sizeof(text) / sizeof(unsigned char), key, outLen);
+	return aes.EncryptECB(text, size, key, outLen);
 }
 
-unsigned char* decryptECB(unsigned char* ciphertext, unsigned char* key) {
-	AES aes(128);
+unsigned char* decryptECB(unsigned char* ciphertext, unsigned int size, unsigned char* key) {
+	AES aes(BLOCK_SIZE_BYTES);
 	unsigned int outLen = 0;
-	return aes.EncryptECB(ciphertext, sizeof(ciphertext) / sizeof(unsigned char), key, outLen);
+	return aes.DecryptECB(ciphertext, size, key);
 }
 
-unsigned char* encryptCBC(unsigned char* text, unsigned char* key) {
-	AES aes(128);
+cbc_data encryptCBC(unsigned char* chyphertext, unsigned int size, unsigned char* key) {
+	AES aes(BLOCK_SIZE_BYTES);
 
-	unsigned char* iv = generateIV();
+	unsigned char* iv = generate();
+	unsigned char* prev_C = iv;
 
-	unsigned char** splitted_text = splitTextToBlocks(text);
+	unsigned int blocks_number = size / BLOCK_SIZE;
+	unsigned char** P = splitTextToBlocks(chyphertext, blocks_number);
 
-	unsigned char* ciphertext = new unsigned char[sizeof(text) / sizeof(unsigned char) + sizeof(iv) / sizeof(unsigned char)];
+	unsigned char* ciphertext = new unsigned char[size];
 
-	memcpy(ciphertext, iv, sizeof(iv));
+	unsigned int outLen = 0;
 
-	for (unsigned int block = 0; block < sizeof(splitted_text) / sizeof(splitted_text[0]); block++) {
-		int block_size = sizeof(splitted_text[block]);
-
-		for (unsigned int byte = 0; byte < block_size; byte++) {
-			splitted_text[block][byte] %= iv[byte];
+	for (unsigned int block = 0; block < blocks_number; block++) {
+		unsigned char* C = new unsigned char[BLOCK_SIZE];
+		for (unsigned int byte = 0; byte < BLOCK_SIZE; byte++) {
+			C[byte] = P[block][byte] ^ prev_C[byte];
 		}
 
-		memcpy(ciphertext + sizeof(iv) + block * BLOCK_SIZE, splitted_text[block], block_size);
+		C = aes.EncryptECB(C, BLOCK_SIZE, key, outLen);
 
-		iv = splitted_text[block];
+		memcpy(ciphertext + block * BLOCK_SIZE, C, BLOCK_SIZE);
+		
+		if (prev_C != iv) {
+			delete(prev_C);
+		}
+		
+		prev_C = C;
 	}
 
-	return ciphertext;
+	cbc_data result = { ciphertext, iv };
+
+	return result;
 }
 
-unsigned char* generateIV() {
+unsigned char* decryptCBC(cbc_data cbc, unsigned int size, unsigned char* key) {
+	AES aes(BLOCK_SIZE_BYTES);
 
-}
+	unsigned int blocks_number = size / BLOCK_SIZE;
+	unsigned char** C = splitTextToBlocks(cbc.bytes, blocks_number);
 
-unsigned char* decryptCBC(unsigned char* ciphertext, unsigned char* key) {
-	AES aes(128);
+	unsigned char* prev_C = cbc.iv;
 
-	unsigned char** splitted_text = splitTextToBlocks(ciphertext);
+	unsigned char* text = new unsigned char[size];
 
-	unsigned char* iv = splitted_text[0];
-
-	unsigned char* text = new unsigned char[sizeof(ciphertext) / sizeof(unsigned char) - sizeof(iv) / sizeof(unsigned char)];
-
-	unsigned int overlap = 0;
-
-	for (unsigned int block = 1; block < sizeof(splitted_text); block++) {
-		unsigned char* next_iv = splitted_text[block];
+	for (unsigned int block = 0; block < blocks_number; block++) {
+		unsigned char* P = aes.DecryptECB(C[block], BLOCK_SIZE, key);
 
 		for (unsigned int byte = 0; byte < BLOCK_SIZE; byte++) {
-			splitted_text[block][byte] %= iv[byte];
+			P[byte] ^= prev_C[byte];
 		}
 
-		memcpy(text + overlap, splitted_text[block], BLOCK_SIZE);
+		memcpy(text + (block * BLOCK_SIZE), P, BLOCK_SIZE);
 
-		iv = next_iv;
+		prev_C = C[block];
+
+		delete P;
 	}
 
 	return text;
 }
 
-unsigned char** splitTextToBlocks(unsigned char* text) {
-	unsigned int blocks_number = sizeof(text) / BLOCK_SIZE;
-
-	unsigned int overlap = 0;
-
+unsigned char** splitTextToBlocks(unsigned char* text, unsigned int blocks_number) {
 	unsigned char** splitted_text = new unsigned char*[blocks_number];
-	for (int i = 0; i < blocks_number; i++) {
-		splitted_text[i] = new unsigned char[BLOCK_SIZE];
-		memcpy(text + i * BLOCK_SIZE, splitted_text[i], BLOCK_SIZE);
+	for (unsigned int i = 0; i < blocks_number; i++) {
+		splitted_text[i] = text + (i * BLOCK_SIZE);
 	}
 
 	return splitted_text;
